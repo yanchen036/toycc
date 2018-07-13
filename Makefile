97a90a32010fea9fa1f1173d365a81618100b21f
yanchen036 <yanchen036@gmail.com>
2018-07-13 16:50:09 +0800

    add binary expression

--- Makefile
+++ Makefile
@@ -2,12 +2,12 @@ VPATH = include src
 OBJ = main.o
 INCLUDE = 'include'
 
-main : $(OBJ)
-	g++ -o main $^
+toycc : $(OBJ)
+	g++ $^ -o $@
 
 main.o : main.cc
 	g++ -c $< -o $@
 
 .PHONY : clean
 clean:
-	rm $(OBJ) main
+	rm $(OBJ) toycc
--- src/main.cc
+++ src/main.cc
@@ -1,47 +1,41 @@
 #include <cstdio>
 #include <string>
 
-FILE* in_s = stdin;
-FILE* out_s = stdout;
+FILE *in_s = stdin;
+FILE *out_s = stdout;
 
 const char TAB = '\t';
 char Look;
 
-void GetChar() {
-  Look = std::getc(in_s);
-}
+void GetChar() { Look = std::getc(in_s); }
 
-void Error(const std::string& s) {
+void Error(const std::string &s) {
   fprintf(out_s, "\nError: %s.\n", s.c_str());
 }
 
-void Abort(const std::string& s) {
+void Abort(const std::string &s) {
   Error(s);
   std::abort();
 }
 
-void Expected(const std::string& s) {
-  Abort(s + " Expected");
-}
+void Expected(const std::string &s) { Abort(s + " Expected"); }
 
 void Match(char x) {
   if (x == Look)
     GetChar();
   else
-    Expected("\'" + std::string(1,x) + "\'");
+    Expected("\'" + std::string(1, x) + "\'");
 }
 
 bool IsAlpha(char c) {
   return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 }
 
-bool IsDigit(char c) {
-  return c >= '0' && c <= '9';
-}
+bool IsDigit(char c) { return c >= '0' && c <= '9'; }
 
 char UpCase(char c) {
   if (c >= 'a' && c <= 'z')
-    return c-32;
+    return c - 32;
   else
     return c;
 }
@@ -62,20 +56,48 @@ char GetNum() {
   return num;
 }
 
-void Emit(const std::string& s) {
-  fprintf(out_s, "%c%s", TAB, s.c_str());
-}
+void Emit(const std::string &s) { fprintf(out_s, "%c%s", TAB, s.c_str()); }
 
-void EmitLn(const std::string& s) {
+void EmitLn(const std::string &s) {
   Emit(s);
   fprintf(out_s, "\n");
 }
 
-void Init() {
-  GetChar();
+void Init() { GetChar(); }
+
+// expression
+void Term() { EmitLn("MOVE #" + std::string(1, GetNum()) + ",D0"); }
+
+void Add() {
+  Match('+');
+  Term();
+  EmitLn("ADD D1,D0");
+}
+
+void Subtract() {
+  Match('-');
+  Term();
+  EmitLn("SUB D1,D0");
+  EmitLn("NEG D0");
+}
+
+void Expression() {
+  Term();
+  EmitLn("MOVE D0,D1");
+  switch (Look) {
+  case '+':
+    Add();
+    break;
+  case '-':
+    Subtract();
+    break;
+  default:
+    Expected("Addop");
+  }
 }
 
 int main() {
   Init();
+  Expression();
   return 0;
 }
